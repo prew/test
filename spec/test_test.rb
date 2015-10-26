ff4fb2b4df7ad94bc8896e4e6f5a5863e0b375ff
Lin Xiangyu <lxyweb@gmail.com>
2015-10-26 23:39:50 +0800

    update

---
+++ spec/test_test.rb
@@ -0,0 +1,8 @@
+require "minitest/autorun"
+
+class TestMeme < Minitest::Test
+  def test_that_will_be_success
+  	puts 'a'
+    assert_equal "hello", "hello"
+  end
+end
---
+++ test/test_test.rb
@@ -0,0 +1,7 @@
+require "minitest/autorun"
+
+class TestMeme < Minitest::Test
+  def test_that_will_be_success
+    assert_equal "hello", "hello"
+  end
+end
